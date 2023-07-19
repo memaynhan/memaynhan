77137e058f5e71b54f7334a5be72e21fe7a04aff
Baothang <138952885+memaynhan@users.noreply.github.com>
2023-07-19 11:32:44 +0700

    Add files via upload

---
+++ BAI6/Form1.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace BAI6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtma = new System.Windows.Forms.TextBox();
+            this.txttensv = new System.Windows.Forms.TextBox();
+            this.txtdiem = new System.Windows.Forms.TextBox();
+            this.txtmakhoa = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(90, 40);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "|<";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(245, 40);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "<<";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(541, 39);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = ">>";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(704, 35);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 3;
+            this.button4.Text = ">|";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(158, 134);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 16);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "MA SV";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(150, 195);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "TEN SV";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(150, 251);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "DIEM SV";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(150, 311);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 16);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "MA KHOA";
+            // 
+            // txtma
+            // 
+            this.txtma.Location = new System.Drawing.Point(245, 134);
+            this.txtma.Name = "txtma";
+            this.txtma.Size = new System.Drawing.Size(464, 22);
+            this.txtma.TabIndex = 5;
+            // 
+            // txttensv
+            // 
+            this.txttensv.Location = new System.Drawing.Point(245, 189);
+            this.txttensv.Name = "txttensv";
+            this.txttensv.Size = new System.Drawing.Size(446, 22);
+            this.txttensv.TabIndex = 6;
+            // 
+            // txtdiem
+            // 
+            this.txtdiem.Location = new System.Drawing.Point(245, 245);
+            this.txtdiem.Name = "txtdiem";
+            this.txtdiem.Size = new System.Drawing.Size(439, 22);
+            this.txtdiem.TabIndex = 7;
+            // 
+            // txtmakhoa
+            // 
+            this.txtmakhoa.Location = new System.Drawing.Point(245, 305);
+            this.txtmakhoa.Name = "txtmakhoa";
+            this.txtmakhoa.Size = new System.Drawing.Size(446, 22);
+            this.txtmakhoa.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(397, 42);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(103, 16);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "VITRI/TONGSO";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(293, 364);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(140, 37);
+            this.button5.TabIndex = 9;
+            this.button5.Text = "LUU";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(484, 364);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(176, 36);
+            this.button6.TabIndex = 10;
+            this.button6.Text = "THEM";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(715, 364);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(153, 37);
+            this.button7.TabIndex = 11;
+            this.button7.Text = "XOA";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1149, 450);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.txtmakhoa);
+            this.Controls.Add(this.txtdiem);
+            this.Controls.Add(this.txttensv);
+            this.Controls.Add(this.txtma);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtma;
+        private System.Windows.Forms.TextBox txttensv;
+        private System.Windows.Forms.TextBox txtdiem;
+        private System.Windows.Forms.TextBox txtmakhoa;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+    }
+}
+
---
+++ BAI6/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BAI6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        String strcnn = "Data Source=LAPTOP-HF25TITB;Initial Catalog=QLSV;Integrated Security=True";
+        BindingSource bs = null;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SqlConnection cnn = new SqlConnection(strcnn);
+            if( cnn == null)
+                cnn = new SqlConnection(strcnn);    
+            SqlDataAdapter adt = new SqlDataAdapter("Select *from SinhVien",cnn);
+            SqlCommandBuilder buider = new SqlCommandBuilder(adt);
+            DataSet ds = new DataSet();
+            adt.Fill(ds, "SinhVien");
+            bs = new BindingSource(ds, "SinhVien");
+            txtma.DataBindings.Add("text", bs, "MSSV");
+            txttensv.DataBindings.Add("text", bs, "TENSV");
+            txtdiem.DataBindings.Add("text", bs, "DTB");
+            txtmakhoa.DataBindings.Add("text", bs, "MAKHOA");
+
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(bs.Position >0) { bs.Position--; }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (bs.Position < bs.Count - 1) { bs.Position++; }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            bs.MoveLast();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bs.MoveFirst();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            bs.EndEdit();
+            MessageBox.Show(" LUU THANH CONG");
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            bs.AddNew();
+            MessageBox.Show("them THANH CONG");
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            bs.RemoveCurrent();
+            MessageBox.Show("xoa THANH CONG");
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+            label5.Text = (bs.Position +1) + "/" + bs.Count;
+        }
+    }
+}
+
